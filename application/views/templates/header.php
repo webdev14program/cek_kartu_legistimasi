be8b24e33d315462ab8dff63c32e6ce980e00130
Rahmadika S Setiawan <dikakewren@outlook.com>
2024-03-12 11:50:03 +0700

    MVC UPDATE

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,11 +3,15 @@
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Adminiistrator SMK TUnas Harapan Jakarta Barat">
+    <meta name="author" content="Rahmadika S Setiawan,S.Kom">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Selamat Datang Di Website Resmi SMK Tunas Harapan Jakarta Barat</title>
+    <link rel="icon" href="https://smkth-jakbar.com/assets/images/logo.png">
+
 </head>
